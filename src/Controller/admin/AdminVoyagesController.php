3d4fd350f17f64f7d13727e413a8d60eef2fb1d6
pilou <pilou@DESKTOP-F088J5M>
2023-01-03 15:59:11 +0100

    Phase 12: ORM (suppresion d'un enregistrement).

--- src/Controller/admin/AdminVoyagesController.php
+++ src/Controller/admin/AdminVoyagesController.php
@@ -7,9 +7,10 @@
 
 namespace App\Controller\admin;
 
+use App\Entity\Visite;
 use App\Repository\VisiteRepository;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,8 +22,37 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminVoyagesController extends AbstractController {
     
     /**
+     * @Route("/admin", name ="admin.voyages")
+     * @return Response
+    */
+    public function index(): Response{
+        $visites = $this->repository->findAllOrderBy('datecreation', 'DESC');
+        return $this->render("admin/admin.voyages.html.twig", [
+            'visites' => $visites
+        ]);
+    }
+    
+    /**
      * 
      * @var VisiteRepository
      */
     private $repository;
+    
+    /**
+     * 
+     * @param VisiteRepository $repository
+     */
+    public function __construct(VisiteRepository $repository) {
+        $this->repository = $repository;
+    }
+    
+    /**
+     * @Route("/admin/suppr/{id}", name="admin.voyage.suppr")
+     * @param Visite $visite
+     * @return Response
+     */
+    public function suppr(Visite $visite): Response{
+        $this->repository->remove($visite, true);
+        return $this->redirectToRoute('admin.voyages');
+    }
 }
