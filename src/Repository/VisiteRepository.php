013e49a46b3b3a25ae8ab2488705df943e7799eb
pilou <pilou@DESKTOP-F088J5M>
2023-01-03 13:46:14 +0100

    Phase 10: ORM (filtrage des enregistrements).

--- src/Repository/VisiteRepository.php
+++ src/Repository/VisiteRepository.php
@@ -77,4 +77,27 @@ class VisiteRepository extends ServiceEntityRepository
                     ->getResult();
         }
         
+        /**
+         * Enregistrements dont un champ est égal à une valeur
+         * out tous les enregistrements si la valeur est vide
+         * @param type $champ
+         * @param type $valeur
+         * @return Visite[]
+         */
+        public function findByEqualValue($champ, $valeur) : array {
+            if ($valeur==""){
+                return $this->createQueryBuilder('v') // allias de la table
+                        ->orderBy ('v.'.$champ,'ASC')
+                        ->getQuery()
+                        ->getResult();
+            } else {
+                return $this->createQueryBuilder('v') // allias de la table
+                        ->where('v.'.$champ.'=:valeur')
+                        ->setParameter('valeur', $valeur)
+                        ->orderBy('v.datecreation', 'DESC')
+                        ->getQuery()
+                        ->getResult();
+            }
+        }
+        
 }
